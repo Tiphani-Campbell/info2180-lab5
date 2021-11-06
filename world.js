4e19b83150e8cda77ccdc4c690b645bc78382594
Tiphani-Campbell <tiphani.campbell@gmail.com>
2021-11-06 14:12:03 -0500

    Completed exercise 5

--- world.js
+++ world.js
@@ -1,6 +1,7 @@
 window.addEventListener("DOMContentLoaded", () =>{
 
     var btn = document.getElementById("lookup");
+    var citybtn = document.getElementById("cities");
     
     var httpRequest;
 
@@ -9,18 +10,29 @@ window.addEventListener("DOMContentLoaded", () =>{
         httpRequest = new XMLHttpRequest();
         var input = document.getElementById("country").value;
         var url = "world.php" + "?country=" + input;
-        httpRequest.onreadystatechange = findCountry;
+        httpRequest.onreadystatechange = findResults;
         httpRequest.open('GET', url);
         httpRequest.send();
 
     });//end button action listener
 
-    var findCountry = function (){
+    citybtn.addEventListener('click', (e) =>{
+        e.preventDefault();
+        httpRequest = new XMLHttpRequest();
+        var input = document.getElementById("country").value;
+        var url = "world.php" + "?country=" + input + "&context=cities";
+        httpRequest.onreadystatechange=findResults;
+        httpRequest.open('GET',url);
+        httpRequest.send();
+
+    });//end city button action listener
+
+    var findResults = function (){
         if(httpRequest.readyState===XMLHttpRequest.DONE){
             if(httpRequest.status === 200){
                 var response = httpRequest.responseText;
-                console.log(response);
                 var result = document.getElementById("result");
+                result.setAttribute("tag","table");
                 result.innerHTML=response;
             }else{
                 alert("There was a problem with the request");
--- world.php
+++ world.php
@@ -7,17 +7,55 @@ $dbname = 'world';
 $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
 if($_SERVER['REQUEST_METHOD']=== 'GET'){
   $country = filter_var($_GET['country'], FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-  if(isset($country) or strlen($country) !== 0){
+  if(isset($country) or strlen($country)!==0){
+    if((isset($country) or strlen($country)!==0) and (!isset($_GET['context']) or strlen($_GET['context'])===0)){
     $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
     $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-    echo "<ul>";
+    echo "<table>";
+    echo "<tr>";
+    echo "<th>Country Name</th>";
+    echo "<th>Continent</th>";
+    echo "<th>Independence</th>";
+    echo "<th>Head of State</th>";
+    echo "<tr>";
     foreach($results as $row){
-      echo "<li>" . $row['name'] . ' is ruled by ' . $row['head_of_state'] . "</li>";
+     $name = $row['name'];
+     $continent = $row['continent'];
+     $independence = $row['independence_year'];
+     $hos = $row['head_of_state'];
+     echo "<tr>";
+     echo "<td>$name</td>";
+     echo "<td>$continent</td>";
+     echo "<td>$independence</td>";
+     echo "<td>$hos</td>";
+     echo "</tr>";
     }
-    echo "</ul>";
+    echo "</table>";
 
+  }else{
+    $stmt = $conn->query("SELECT c.name as city, c.district, c.population, cs.name as country FROM cities c join countries cs on c.country_code=cs.code WHERE cs.name='$country'");
+    $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    echo "<table>";
+    echo "<tr>";
+    echo "<th>Name</th>";
+    echo "<th>District</th>";
+    echo "<th>Population</th>";
+    echo "</tr>";
+    foreach($results as $row){
+      $city = $row['city'];
+      $district = $row['district'];
+      $population = $row['population'];
+      echo "<tr>";
+      echo "<td>$city</td>";
+      echo "<td>$district</td>";
+      echo "<td>$population</td>";
+      echo "</tr>";
+    }
+    echo "</table>";
   }
+}
 }
 
 
